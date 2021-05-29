bb2aba9f96199df01385970024574198b5648d0e
prateekrao <r_prateek@icloud.com>
2021-05-29 17:37:19 +0530

    spotify updated

--- spot.py
+++ spot.py
@@ -3,6 +3,7 @@ import time
 import spotipy
 
 from spotipy.oauth2 import SpotifyClientCredentials
+import random
 
 client_id = '8be5f0367b7a4b54b730b33faa7c1b2c'
 client_secret = '0bc78da67fa44fe985c2586aa74d7e1d'
@@ -22,33 +23,18 @@ def get_track_ids(playlist_id):
         music_id_list.append(music_track['id'])
     return music_id_list
 
-#function to extract all details of track
-
-def get_track_data(track_id):
-    meta = sp.track(track_id)
-
-    track_details = {'name':meta['name'], 'album':meta['album']['name'], 
-                        'artist':meta['album']['artists'][0]['name'], 'release_date': meta['album']['release_date'],
-                        'duration_in_mins':round((meta['duration_ms']*0.001)/60, 2)}
-    return track_details
-
 #get the ids for all songs
 
 playlist_id = input('Enter the playlist id')
 track_ids = get_track_ids(playlist_id)
-print(len(track_ids))
 
-print(track_ids)
+random.shuffle(track_ids)
 
-tracks = []
+embed = '<iframe src="https://open.spotify.com/embed/track/{}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'.format(track_ids[0])
 
-for i in range(len(track_ids)):
-    time.sleep(.5)
-    track = get_track_data(track_ids[i])
-    tracks.append(track)
+print(embed)
 
-with open('spotify_data.json', 'w') as outfile:
-    json.dump(tracks, outfile, indent= 4)
+
 
 
 
